0e2c5886530a30a7f40e7a2af409f1424ea47271
David Read <david.read@hackneyworkshop.com>
2016-11-25 17:08:32 +0000

    Improve dataset migration. Add convenience of the virtualenv being activated automatically and $CKAN_INI set.

--- IMPORT.md
+++ IMPORT.md
@@ -2,33 +2,55 @@
 
 To import existing data into this CKAN from DGU, you should take the following steps.
 
-1. Ensure that you have a working CKAN install with a CKAN Virtualenv available
+1. ssh into the vagrant box of this repo:
 
-2. Connect and activate the Virtualenv
+    vagrant ssh
 
-3. If you have not installed it before, ```sudo /usr/lib/ckan/bin/pip install ckanapi```
+2. Activate the Virtualenv:
 
-4. You will also need an apikey from the source server to download users.
+    . /usr/lib/ckan/bin/activate
 
-5. Ensure the following command returns []
-   ```ckanapi action package_list -r http://127.0.0.1:8080```
+3. You will also need an apikey from the source server to download users.
 
-6. Dump data from remote CKAN to local files. This takes a long time.
+4. Ensure the local db is empty - returns [] for these:
+
+       ckanapi action package_list -r http://127.0.0.1:8080
+       ckanapi action organization_list -r http://127.0.0.1:8080
+
+   And this should only have the "dgu" user:
+
+       ckanapi action user_list -r http://127.0.0.1:8080
+
+   If you need to wipe the db:
+
+       # DANGER
+       sudo service apache2 restart && sudo -u postgres dropdb ckan
+       sudo -u postgres createdb ckan -E UTF-8 -T template_postgis -O dgu
+       paster --plugin=ckan db init -c /etc/ckan/ckan.ini
+
+5. Get data from remote CKAN to local files. This takes a long time.
 
 ```
-cd ~
-mkdir dumps
-cd dumps
-ckanapi dump organizations --all -O organizations.jsonl.gz -z -p 4  -r https://test.data.gov.uk
-ckanapi dump datasets --all -O datasets.jsonl.gz -z  -p 4  -r https://test.data.gov.uk
+mkdir ~/dumps
+cd ~/dumps
+curl https://data.gov.uk/data/dumps/data.gov.uk-ckan-meta-data-latest.v2.jsonl.gz -o datasets.jsonl.gz
+curl https://data.gov.uk/data/dumps/data.gov.uk-ckan-meta-data-latest.organizations.jsonl.gz -o organizations.jsonl.gz
 
-# Following command does not complete :(  
+# (Or create the dumps manually:
+#   ckanapi dump organizations --all -O organizations.jsonl.gz -z -p 4  -r https://test.data.gov.uk
+#   ckanapi dump datasets --all -O datasets.jsonl.gz -z  -p 4  -r https://test.data.gov.uk
+# )
+
+# Following command does not complete :(
 # ckanapi dump users --all -O users -z  -p 4  -r https://test.data.gov.uk
-
-ckanapi load organizations -I organizations.jsonl.gz -z -p 3 -c /etc/ckan/ckan.ini
-
-# Clean datasets file before importing
-python /vagrant/import/clean_export.py -s datasets.jsonl.gz -o fixed_datasets.jsonl.gz
-ckanapi load datasets -I fixed_datasets.jsonl.gz -z -p 3 -c /etc/ckan/ckan.ini
-
 ```
+
+6. Load the organization data into this box's CKAN:
+
+    ckanapi load organizations -I organizations.jsonl.gz -z -p 3 -c /etc/ckan/ckan.ini
+
+7. Migrate the dataset data and then import:
+
+    python /vagrant/import/migrate_datasets.py -s datasets.jsonl.gz -o datasets_migrated.jsonl.gz
+    ckanapi load datasets -I datasets_migrated.jsonl.gz -z -p 3 -c /etc/ckan/ckan.ini
+
---
+++ import/migrate_datasets.py
@@ -0,0 +1,107 @@
+"""
+Migrates the DGU dataset metadata from the old DGU-customized CKAN to be
+suitable for loading into a vanilla CKAN. Some of the fields prevented
+datasets to be loaded.
+
+Run this script by passing the "-s datasets.jsonl.gz" parameter and it will
+generate a new jsonl.gz in the same folder as the source file.
+"""
+import argparse
+import json
+import os
+import sys
+import gzip
+import re
+
+def process(source, output):
+    with gzip.open(source, 'rb') as input_f, \
+            gzip.open(output, 'wb') as output_f:
+        lines = input_f.readlines()
+        for line in lines:
+            new_entry = clean_and_write(line)
+            output_f.write(new_entry + "\n")
+
+
+def clean_and_write(dataset_json):
+    dataset = json.loads(dataset_json)
+
+    # These split-up resources are repeats of the 'resources' key and
+    # cause problems on load - just get rid of them
+    keys_to_delete = ['individual_resources', 'additional_resources',
+                      'timeseries_resources']
+
+    # Tags on old DGU had looser validation than default ckan
+    tags_to_delete = []
+    for tag_dict in dataset['tags'][:]:
+        tag = tag_dict['name']
+        # get rid of tags of 1 character e.g. B (chemical name for Boron)
+        if len(tag) < 2:
+            tags_to_delete.append(tag_dict)
+            continue
+        # munge to get rid of disallowed symbols
+        munged_tag = munge_tag(tag)
+        if munged_tag != tag:
+            tag_dict['name'] = munged_tag
+    for tag in tags_to_delete:
+        dataset['tags'].remove(tag)
+
+    # Shunt custom fields into extras (while we work out what to do with them)
+    for key in ['schema', 'codelist',
+                'archival', 'qa']:
+        if key not in dataset:
+            continue
+        # delete an extra in that name if it exists
+        for extra in dataset['extras'][:]:
+            if extra['key'] == key:
+                dataset['extras'].remove(extra)
+        dataset['extras'].append(
+            dict(
+                key=key,
+                value=json.dumps(dataset[key])
+                ))
+        keys_to_delete.append(key)
+
+    # Delete keys
+    for key in keys_to_delete:
+        if key in dataset:
+            del dataset[key]
+
+    return json.dumps(dataset)
+
+
+MAX_TAG_LENGTH = 100
+MIN_TAG_LENGTH = 2
+
+def munge_tag(tag):
+    tag = re.sub(r'[^a-zA-Z0-9\- ]', '', tag).replace(' ', '-')
+    tag = _munge_to_length(tag, MIN_TAG_LENGTH, MAX_TAG_LENGTH)
+    return tag
+
+def _munge_to_length(string, min_length, max_length):
+    '''Pad/truncates a string'''
+    if len(string) < min_length:
+        string += '_' * (min_length - len(string))
+    if len(string) > max_length:
+        string = string[:max_length]
+    return string
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cleanup the DGU export')
+    parser.add_argument('--source', '-s', dest='source',
+                        help='Specify the location of the source file')
+    parser.add_argument('--output', '-o', dest='output',
+                        help='Specify the location of the output file')
+
+    args = parser.parse_args()
+    if not args.source or not args.output:
+        print "Both source and output files are required\n"
+        print parser.print_help()
+        sys.exit(1)
+
+    if not os.path.exists(args.source):
+        print "Source file {0} could not be found\n".format(args.source)
+        print parser.print_help()
+        sys.exit(1)
+
+    process(args.source, args.output)
