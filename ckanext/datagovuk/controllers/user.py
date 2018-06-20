4aaef692d59448d7f5ef937f67ee913ae5cadd32
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2018-06-20 10:14:15 +0100

    Remove news feed from user dashboard and replace default logged in page

--- ckanext/datagovuk/controllers/user.py
+++ ckanext/datagovuk/controllers/user.py
@@ -1,7 +1,16 @@
 from ckan.controllers.user import UserController
+import ckan.lib.helpers as h
+
+from ckan.common import _, c, request, response
 
 class UserController(UserController):
     def _new_form_to_db_schema(self):
         from ckanext.datagovuk.schema import user_new_form_schema
         return user_new_form_schema()
 
+    def me(self, locale=None):
+        if not c.user:
+            h.redirect_to(locale=locale, controller='user', action='login',
+                          id=None)
+        user_ref = c.userobj.get_reference_preferred_for_uri()
+        h.redirect_to(locale=locale, controller='user', action='dashboard_datasets')
