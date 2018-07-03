2d1307edfc7deadf6759c23327243faff7fc0685
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2018-07-03 08:40:59 +0100

    Add tests for all DGU custom routes

--- ckanext/datagovuk/plugin.py
+++ ckanext/datagovuk/plugin.py
@@ -162,7 +162,7 @@ class DatagovukPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, Defau
             m.connect('/user/edit', action='edit')
             m.connect('/user/edit/{id:.*}', action='edit')
             m.connect('/user/reset', action='request_reset')
-        route_map.connect('/healthcheck', controller=healthcheck_controller, action='healthcheck')
+        route_map.connect('healthcheck', '/healthcheck', controller=healthcheck_controller, action='healthcheck')
         return route_map
 
     def after_map(self, route_map):
