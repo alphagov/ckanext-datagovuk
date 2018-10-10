d591b8b914a6688fd57c20eee21200e31c9e4f62
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2018-10-10 10:32:12 +0100

    Install the S3 plugin during build

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -4,6 +4,7 @@ venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckan/2.
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-harvest/v1.1.0/pip-requirements.txt)
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-dcat/master/requirements.txt)
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-spatial/master/pip-requirements.txt)
+venv/bin.pip install -U $(curl -s https://raw.githubusercontent.com/alphagov/ckanext-s3-resources/master/requirements.txt)
 venv/bin/pip install -Ue 'git+https://github.com/ckan/ckan.git@ckan-2.7.0#egg=ckan'
 venv/bin/pip install -r venv/src/ckan/requirements.txt
 venv/bin/pip install -r requirements.txt
