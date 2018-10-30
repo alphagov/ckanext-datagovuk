2ce3dc44eef283167bb56e45991b370165eea207
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-10-30 12:22:25 +0000

    Move the test runner script into `bin`
    This is where the other executables are.

--- bin/jenkins-tests.sh
+++ bin/jenkins-tests.sh
@@ -3,4 +3,4 @@ venv/bin/pip install -r venv/src/ckan/dev-requirements.txt
 dropdb ckan_test; createdb ckan_test
 venv/bin/python setup.py develop
 paster --plugin=ckan db init -c test-jenkins.ini
-run-tests.sh
+bin/run-tests.sh
