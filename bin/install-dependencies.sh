f33c756a5fdf86d04ac396607a8b35c8886ea66e
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-10-30 11:37:46 +0000

    Pin ckanext-harvest to a merge commit
    This commit merges in a fix for a behaviour we've had regarding the base harvester class.
    
    See https://github.com/ckan/ckanext-harvest/pull/338
    
    This can be switched to a release tag pin once a new release is.. released.

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckan/2.7/requirement-setuptools.txt)
-venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-harvest/v1.1.0/pip-requirements.txt)
+venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-harvest/c21c1627992cbcf0043b7b6873ff899f6f66767f/pip-requirements.txt)
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-dcat/master/requirements.txt)
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/ckan/ckanext-spatial/master/pip-requirements.txt)
 venv/bin/pip install -U $(curl -s https://raw.githubusercontent.com/alphagov/ckanext-s3-resources/master/requirements.txt)
