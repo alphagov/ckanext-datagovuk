4b795c8867fec1ee093e99727e2e06390cca32ad
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2018-11-01 10:32:00 +0000

    Remove inheritance from CKAN for testing
    There were two test configurations: `test.ini` and `test-jenkins.ini`.
    These both inherited another configuration from core CKAN, which is
    located in a different location for each test environment.
    
    Changes to the test configuration had to be made in both files, which
    resulted in duplication of effort and possibility for things to be
    missed.
    
    This change removes the inheritance from core CKAN and removes
    duplication from `test-jenkins.ini`, which now inherits from the main
    test configuration.

--- bin/jenkins-tests.sh
+++ bin/jenkins-tests.sh
@@ -4,6 +4,7 @@ venv/bin/pip install -r dev-requirements.txt
 dropdb ckan_test; createdb ckan_test
 venv/bin/python setup.py develop
 paster --plugin=ckan db init -c test-jenkins.ini
+paster --plugin=ckanext-harvest harvester initdb -c test-jenkins.ini
 
 export TEST_CKAN_INI=test-jenkins.ini
 bin/run-tests.sh
