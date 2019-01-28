f60a29e4b5bb2e834845224c0ff7ee1bdea8c47f
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-01-28 14:12:29 +0000

    Better schema key error

--- ckanext/datagovuk/logic/validators.py
+++ ckanext/datagovuk/logic/validators.py
@@ -21,3 +21,12 @@ def user_password_validator(key, data, errors, context):
         errors[('password',)].append(_('Your password must be 8 characters or longer'))
     elif not bool(re.search('[A-Z]', value)) or not bool(re.search('[a-z]', value)):
         errors[('password',)].append(_('Your password must contain at least one upper and one lower case character'))
+
+# This replaces the stock validator with one with a better error message.
+# Courtesy of our friends to the north.
+# https://github.com/okfn/ckanext-glasgow/blob/master/ckanext/glasgow/logic/validators.py#L387
+def extra_key_not_in_root_schema(key, data, errors, context):
+    for schema_key in context.get('schema_keys', []):
+        if schema_key == data[key]:
+            raise Invalid(_('"{}" clashes with an existing reserved schema field'
+                            ' that has the same name'.format(schema_key)))
