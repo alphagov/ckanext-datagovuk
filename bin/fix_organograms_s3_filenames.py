4fe384558109d061c58e50295c2b5092a038764b
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2019-05-29 15:05:42 +0100

    Fix logging and refine script
    The script will find filenames in S3 that don't end in `senior.csv`,
    'junior.csv' or include `-posts-`, and replace them with the
    correct file structure.
    
    It also replaces the correponding `url` in the database. This ensures
    the url shown for its dataset in Find and CKAN matches, and shows
    the organogram visualisation.

--- bin/fix_organograms_s3_filenames.py
+++ bin/fix_organograms_s3_filenames.py
@@ -1,43 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Requirements - 
+# Requirements, sudo /usr/lib/venv/bin/pip install -
 # boto3
 # pytest
 # pytest-mock
+# psycopg2
 #
 # Set up environment from ckan.ini
 # export AWS_ACCESS_KEY_ID=
 # export AWS_SECRET_ACCESS_KEY=
 # export AWS_REGION=
 # export AWS_ORG_BUCKET=
+# export S3_URL_PREFIX=
+# export POSTGRES_URL=<sqlalchemy.url from ckan.ini>
 #
-# Execute script like this - 
+# Execute script like this -
 # python fix-organograms-s3-filenames.py dry-run
 #
 
 import boto3
 import os
 import sys
+import psycopg2
 
 import logging
 
+import pdb
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 # Create handlers
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler('app.log')
 
+connection = psycopg2.connect(os.environ.get('POSTGRES_URL'))
+
 
 def setupLogging():
-    c_handler.setLevel(logging.INFO)
-    f_handler.setLevel(logging.INFO)
     c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
     f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     c_handler.setFormatter(c_format)
     f_handler.setFormatter(f_format)
     logger.addHandler(c_handler)
     logger.addHandler(f_handler)
+
+    logger.info('====================================================================')
 
 
 def main(dry_run=False):
@@ -51,6 +60,17 @@ def main(dry_run=False):
     )
 
     bucket = s3.Bucket(os.environ.get('AWS_ORG_BUCKET'))
+    logger.info(bucket)
+
+    if dry_run:
+        print('Executing DRY RUN on {}'.format(bucket))
+        logger.info('Executing DRY RUN on {}'.format(bucket))
+        logger.info('====================================================================')
+    else:
+        print('Executing permanent changes on {}'.format(bucket))
+        logger.info('Executing permanent changes on {}'.format(bucket))
+        logger.info('====================================================================')
+
 
     for obj in bucket.objects.all():
         if not obj.key.endswith('.csv'):
@@ -76,20 +96,68 @@ def main(dry_run=False):
             logger.info('invalid new filename {} for {}'.format(new_filename, filename))
             continue
 
-        print('{}renamed from {} to {}'.format(
+        print('{}renamed in S3 from {} to {}'.format(
+            'TO BE ' if dry_run else '', obj.key, new_path))
+        logger.info('{}renamed in S3 from {} to {}'.format(
             'TO BE ' if dry_run else '', obj.key, new_path))
 
         if not dry_run:
-            s3.Object(bucket.name, new_path).copy_from(
+            s3.Object(bucket.name, new_path).copy_from(ACL='public-read',
                 CopySource='{}/{}'.format(bucket.name, obj.key))
 
             objs = list(bucket.objects.filter(Prefix=new_path))
             if objs and objs[0].key == new_path:
-                print('found', len(objs))
+                print('Found', len(objs))
+                logger.info('Found', len(objs))
                 s3.Object(bucket.name, obj.key).delete()
             else:
-                print('not found')
-        # break
+                print('Not found')
+                logger.info('Not found')
+
+        print('Update database record to match url renamed in S3...')
+        logger.info('Update database record to match url renamed in S3...')
+
+        s3_url_prefix = os.environ.get('S3_URL_PREFIX')
+        original_path = obj.key
+
+        original_database_url = '{}{}'.format(s3_url_prefix, original_path)
+        new_database_url = '{}{}'.format(s3_url_prefix, new_path)
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT url FROM resource WHERE url='{}';".format(original_database_url))
+
+        if cursor.fetchone():
+            print('Original url found - {}'.format(original_database_url))
+            logger.info('Original url found - {}'.format(original_database_url))
+
+            sql_statement = "UPDATE resource SET url = REPLACE(url, url, '{}') WHERE URL = '{}';".format(new_database_url,original_database_url)
+
+            if not dry_run:
+                print('Not a dry run - executing statement...')
+                logger.info('Not a dry run - executing statement...')
+
+                cursor.execute('BEGIN TRANSACTION;')
+                cursor.execute(sql_statement)
+                cursor.execute('COMMIT TRANSACTION;')
+
+                cursor.execute("SELECT url FROM resource WHERE url='{}';".format(new_database_url))
+
+                if new_database_url in cursor.fetchone():
+                    print('SUCCESS')
+                    logger.info('SUCCESS')
+                else:
+                    print('FAILED')
+                    logger.info('FAILED')
+            else:
+                print('DRY RUN of SQL statement: {}'.format(sql_statement))
+                logger.info('DRY RUN of SQL statement: {}'.format(sql_statement))
+        else:
+            print('Original url not found in table - {}'.format(original_database_url))
+            logger.info('Original url not found in table -{}'.format(original_database_url))
+
+        print('===============')
+        logger.info('===============')
+        # pdb.set_trace()
 
 
 if __name__ == "__main__":
