a086dc19fb198baaac75e32214be31e6a2b8e1e1
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-01-07 16:56:34 +0000

    Add before create/update hooks

--- ckanext/datagovuk/plugin.py
+++ ckanext/datagovuk/plugin.py
@@ -8,6 +8,7 @@ import ckanext.datagovuk.auth as auth
 import ckanext.datagovuk.schema as schema_defs
 import ckanext.datagovuk.action.create
 import ckanext.datagovuk.action.get
+import ckanext.datagovuk_s3_resources.upload as upload
 
 from ckanext.datagovuk.logic.theme_validator import valid_theme
 from ckanext.harvest.model import HarvestSource, HarvestJob, HarvestObject
@@ -276,7 +277,65 @@ class DatagovukPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, Defau
             sentry_sdk.init(integrations=[FlaskIntegration()])
             return app
 
+    def before_create_or_update(self, context, resource):
+        """before_create_or_update - our own function. NOT a CKAN hook.
+        Contains shared code performed regardless of whether we are
+        creating or updating.
+        """
+
+        # Check if required config options exist
+        logger = logging.getLogger(__name__)
+        if not upload.config_exists():
+            # Log an error
+            logger.error(
+                "Required S3 config options missing. Please check if required config options exist."
+            )
+            raise Exception("Required S3 config options missing")
+        else:
+            # If resource is an API, don't do anything special
+            if resource.get("format") == "API":
+                return
+            elif "upload" not in resource:
+                return
+            elif "upload" in resource and resource["upload"] == "":
+                return
+            # Only upload to S3 if not blacklisted
+            elif not upload.is_blacklisted(resource):
+                upload.upload_resource_to_s3(context, resource)
+            else:
+                # If blacklisted, the resource file is uploaded to CKAN.
+                #
+                # However, in the CKAN source resource_create/resource_update, package_update is
+                # called before the file is uploaded.
+                #
+                # This causes a problem as our package after_update attempts to upload
+                # the package zipfile and it cannot locate the resource file.
+                #
+                # To solve this, we add the field 'resource_create_or_update' into the context object,
+                # and look for it in the package after_update.
+                #
+                # We remove this field from the context object in resource after_create and after_update.
+                #
+                # We don't actually use the value context['resource_create_or_update'], we just check
+                # the existence of 'resource_create_or_update' in context.
+                context["resource_create_or_update"] = True
+
+                logger = logging.getLogger(__name__)
+                logger.info(
+                    "Resource %s from package %s is blacklisted and not uploaded to S3."
+                    % (resource["name"], resource["package_id"])
+                )
+
+    def before_create(self, context, resource):
+        """Runs before resource_create. Modifies resource destructively to put in the S3 URL"""
+        self.before_create_or_update(context, resource)
+
+    def before_update(self, context, _, resource):
+        """Runs before resource_update. Modifies resource destructively to put in the S3 URL"""
+        self.before_create_or_update(context, resource)
+
     import ckanext.datagovuk.ckan_patches  # import does the monkey patching
+
 
 def delete_routes_by_path_startswith(map, path_startswith):
     """
