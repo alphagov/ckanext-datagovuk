813d472c8c5e8722b357b3be9ad4f029f90c68c9
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-02-18 16:25:37 +0000

    Add send password alert mailer

---
+++ ckanext/datagovuk/lib/mailer.py
@@ -0,0 +1,13 @@
+from ckan.lib import mailer
+import ckan.model as model
+
+log = __import__('logging').getLogger(__name__)
+
+
+def send_password_alert(user):
+    try:
+        body = "Your password has been changed, if you haven't done it yourself, let us know"
+        mailer.mail_user(user, 'Your CKAN password has changed', body)
+        log.info('Sent email to alert password change')
+    except mailer.MailerException, e:
+        log.error('Could not send email to alert password change: %s' % unicode(e))
