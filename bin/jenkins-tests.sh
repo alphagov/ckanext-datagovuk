82f3c3e0e119f94600718da520ea2a59094ceeed
Robert Scott <code@humanleg.org.uk>
2020-07-17 14:22:49 +0100

    jenkins test setup: don't fail if ckan_test db can't be dropped
    this is probably just because it doesn't exist. that's fine.
    
    this is a non-ideal way of handling resource setup anyway, and is replaced
    in our 2.8 branch. fix this up temporarily in the 2.7 branch for now.

--- bin/jenkins-tests.sh
+++ bin/jenkins-tests.sh
@@ -4,7 +4,8 @@ set -ex
 
 venv/bin/pip install -r venv/src/ckan/dev-requirements.txt
 venv/bin/pip install -r dev-requirements.txt
-dropdb ckan_test; createdb ckan_test
+dropdb ckan_test || true  # fine if it doesn't exist
+createdb ckan_test
 venv/bin/python setup.py develop
 paster --plugin=ckan db init -c test-jenkins.ini
 paster --plugin=ckanext-harvest harvester initdb -c test-jenkins.ini
