474812ae71c11d7574024d1057be351db59d2a68
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2020-08-25 15:59:20 +0100

    Update harvest sha

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -2,7 +2,7 @@
 
 pip=${1-'/usr/bin/env pip'}
 
-ckan_harvest_sha='73db5f1ee67fc86521cfd1861b5d699ff45b1e33'
+ckan_harvest_sha='07083fe323d28e312612d2a0d6a13e2549a6db1b'
 
 ckan_dcat_sha='b757e5be643a17f08b1bb102348c370abee149d5'
 
