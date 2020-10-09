b5b4c162e9c8bff77cb89e3dc6e6ed0b2367e336
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-10-09 17:52:30 +0100

    Fix test data creation and removal

--- ckanext/datagovuk/lib/mailer.py
+++ ckanext/datagovuk/lib/mailer.py
@@ -9,5 +9,5 @@ def send_password_alert(user):
         body = "Your password has been changed, if you haven't done it yourself, let us know"
         mailer.mail_user(user, 'Your data.gov.uk publisher password has changed', body)
         log.info('Sent email to alert password change')
-    except mailer.MailerException, e:
+    except mailer.MailerException as e:
         log.error('Could not send email to alert password change: %s' % unicode(e))
