ddc1c290a2d61014529eec5c9245c004fdb34054
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-10-20 10:53:30 +0100

    Handle ascii when filtering query
    Ignores characters which can't be converted but as the args will be passed on unchanged this shouldn't affect subsequent processing.

--- ckanext/datagovuk/ckan_patches/query.py
+++ ckanext/datagovuk/ckan_patches/query.py
@@ -6,9 +6,16 @@ BLOCK_TAGS = ['{!xmlparser', '<!doctype']
 logger = logging.getLogger(__name__)
 
 
+def safe_str(obj):
+    try:
+        return str(obj)
+    except UnicodeEncodeError:
+        return obj.encode('ascii', 'ignore').decode('ascii')
+
+
 def run(self, *args, **kwargs):
     for k in args[0]:
-        if all(tag in str(args[0][k]).lower() for tag in BLOCK_TAGS):
+        if all(tag in safe_str(args[0][k]).lower() for tag in BLOCK_TAGS):
             logger.warn('DGU search blocked: %r', args[0][k])
             return {"count": 0, "results": []}
 
