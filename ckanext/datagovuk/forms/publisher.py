0629663469228467009fca31b5aa39023b4e8f57
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-10-26 18:33:04 +0000

    Replace unicode with str

--- ckanext/datagovuk/forms/publisher.py
+++ ckanext/datagovuk/forms/publisher.py
@@ -19,11 +19,11 @@ class PublisherForm(plugins.SingletonPlugin, toolkit.DefaultOrganizationForm):
         schema = group_form_schema()
         for mandatory_extra in ['category']:
             schema.update({mandatory_extra: [toolkit.get_converter('convert_to_extras'),
-                                         unicode]})
+                                         str]})
         for optional_extra in ['contact-name', 'contact-email', 'contact-phone', 'foi-name', 'foi-email', 'foi-web', 'foi-phone']:
             schema.update({optional_extra: [toolkit.get_validator('ignore_missing'),
                                          toolkit.get_converter('convert_to_extras'),
-                                         unicode]})
+                                         str]})
         return schema
 
     def db_to_form_schema(self, package_type=None):
@@ -32,7 +32,7 @@ class PublisherForm(plugins.SingletonPlugin, toolkit.DefaultOrganizationForm):
         for optional_extra in ['category', 'contact-name', 'contact-email', 'contact-phone', 'foi-name', 'foi-email', 'foi-web', 'foi-phone']:
             schema.update({optional_extra: [toolkit.get_converter('convert_from_extras'),
                                          toolkit.get_validator('ignore_missing'),
-                                         unicode]})
+                                         str]})
         schema['num_followers'] = []
         schema['created'] = []
         schema['display_name'] = []
