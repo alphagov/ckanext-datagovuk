cb5424302e2db2501a64f43a10a4cf605b623ae0
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-10-28 18:40:09 +0000

    Update version and python version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.2.0',
+    version='1.0.0',
 
     description='''datagovuk prototype/alpha extension''',
     long_description=long_description,
@@ -43,8 +43,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ],
 
 
