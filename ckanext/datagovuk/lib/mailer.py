e721bb828b1f2495f520c62508ef6a5db92ae676
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-11-04 10:33:38 +0000

    Replace unicode with str syntax for python 3

--- ckanext/datagovuk/lib/mailer.py
+++ ckanext/datagovuk/lib/mailer.py
@@ -10,4 +10,4 @@ def send_password_alert(user):
         mailer.mail_user(user, 'Your data.gov.uk publisher password has changed', body)
         log.info('Sent email to alert password change')
     except mailer.MailerException as e:
-        log.error('Could not send email to alert password change: %s' % unicode(e))
+        log.error('Could not send email to alert password change: %s' % str(e))
