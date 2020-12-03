3aa0474dcf5b830365d7308c240d118070d72534
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-12-03 19:32:25 +0000

    Update set_password patch

--- ckanext/datagovuk/ckan_patches/cli.py
+++ ckanext/datagovuk/ckan_patches/cli.py
@@ -1,19 +1,18 @@
-import ckan.cli.user
+import click
 
+import ckan.cli.user
+from ckan.cli.user import user
+import ckan.model as model
 from ckanext.datagovuk.lib.mailer import send_password_alert
 
-
-def set_password(self):
-    print("*** DGU password")
-    ckan.cli.user.original_set_password(self)
-
-    if len(self.args) > 1:
-        import ckan.model as model
-
-        username = self.args[1]
-        user = model.User.get(username)
-        send_password_alert(user)
+original_set_password = ckan.cli.user.set_password
 
 
-ckan.cli.user.original_set_password = ckan.cli.user.set_password
-ckan.cli.user.set_password = set_password
+@user.command(u'setpass', short_help=u'Set password for the user')
+@click.argument(u'username')
+@click.pass_context
+def set_password(ctx, username):
+    ctx.invoke(original_set_password, username=username)
+
+    _user = model.User.get(username)
+    send_password_alert(_user)
