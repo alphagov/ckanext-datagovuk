fc11788103ad3f79bc9749f30f082108ebf3fa9c
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-12-03 19:32:25 +0000

    Update set_password patch

--- ckanext/datagovuk/tests/test_ckan_patches.py
+++ ckanext/datagovuk/tests/test_ckan_patches.py
@@ -24,34 +24,16 @@ def test_validate_password__wrong_password():
 
 @pytest.mark.ckan_config('ckan.plugins', 'datagovuk test_harvester')
 @pytest.mark.usefixtures("clean_db", "with_plugins")
-# @patch('ckan.cli.user.click.prompt', return_value='newpassword')
 @patch('ckanext.datagovuk.lib.mailer.mailer.mail_user')
-# def test_setpass_sends_email_alert_to_user(cli, mock_mailer):
 def test_setpass_sends_email_alert_to_user(mock_mailer, cli):
     from click.testing import CliRunner
-    from ckanext.datagovuk.ckan_patches.cli import set_password
 
     user_dict = factories.User(password='hello123')
 
-    # runner = CliRunner()
-    # result = runner.invoke(set_password, [user_dict['name']], input="newpass\nnewpass")
-    # result = runner.invoke(set_password, cli, input="newpass\nnewpass")
     from ckan.cli.cli import ckan
-    result = cli.invoke(ckan, ['user', 'setpass', user_dict['name']], input="newpass\nnewpass")
+    cli.invoke(ckan, ['user', 'setpass', user_dict['name']], input="newpass\nnewpass\n")
 
-    # print(result)
-    print(result.output)
 
-    # mocker.patch('ckan.lib.cli.user.click.prompt', return_value='newpassword')
-    # mock_mailer = mocker.patch('ckanext.datagovuk.lib.mailer.mailer.mail_user')
-
-    # user_cmd = ckan.lib.cli.UserCmd('test')
-    # user_cmd.args =['setpass', user_dict['name']]
-
-    # user_cmd.setpass()
-
-    # print(user_dict)
-    # set_password(user_dict['name'])
 
     assert mock_mailer.called
     args, kwargs = mock_mailer.call_args
