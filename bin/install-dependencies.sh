a4d121c77df43c1c89d080c727adf83fc09d41b0
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-12-22 12:36:42 +0000

    Update install-dependencies.sh for CKAN 2.9

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -3,14 +3,15 @@
 pip=${1-'/usr/bin/env pip'}
 
 ckan_harvest_fork='alphagov'
-ckan_harvest_sha='d939cd1a7d767af16816310d8d954d33e2b79c7c'
+ckan_harvest_sha='283ac0498fd22056fb8572d49b25cd8492b7178f'
 
-ckan_dcat_sha='b757e5be643a17f08b1bb102348c370abee149d5'
+ckan_dcat_sha='6b7ec505f303fb18e0eebcebf67130d36b3dca82'
 
 ckan_spatial_fork='alphagov'
-ckan_spatial_sha='46b706549aaf13a4ef2451d6185d7a90a75aeb0f'
+ckan_spatial_sha='d8dd6bd910f08c1f5408487cf2a455fc8ac0a91b'
 
-ckan_sha='ckan-2.8.3-dgu'
+# ckan 2.9.1
+ckan_sha='bb272bfc7167b35cbff8ce48ed815dd4e23597ba'
 
 pycsw_tag='2.4.0'
 
@@ -27,9 +28,9 @@ $pip install $pipopt -U "git+https://github.com/ckan/ckanext-dcat.git@$ckan_dcat
 $pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/$ckan_spatial_fork/ckanext-spatial/$ckan_spatial_sha/pip-requirements.txt)
 $pip install $pipopt -U "git+https://github.com/$ckan_spatial_fork/ckanext-spatial.git@$ckan_spatial_sha#egg=ckanext-spatial"
 
-$pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/alphagov/ckan/$ckan_sha/requirement-setuptools.txt)
-$pip install $pipopt -r https://raw.githubusercontent.com/alphagov/ckan/$ckan_sha/requirements.txt
-$pip install $pipopt -Ue "git+https://github.com/alphagov/ckan.git@$ckan_sha#egg=ckan"
+$pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirement-setuptools.txt)
+$pip install $pipopt -r https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirements.txt
+$pip install $pipopt -Ue "git+https://github.com/ckan/ckan.git@$ckan_sha#egg=ckan"
 
 $pip install $pipopt -r requirements.txt
 
