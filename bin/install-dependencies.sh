e102a1b2503ca76090bdf6a98e39f0a5a21fd102
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-12-23 10:23:40 +0000

    Update install dependencies and test scripts

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -19,6 +19,10 @@ pipopt='--exists-action=b'
 
 $pip install $pipopt -U numpy==1.16.4
 
+$pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirement-setuptools.txt)
+$pip install $pipopt -r https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirements.txt
+$pip install $pipopt -Ue "git+https://github.com/ckan/ckan.git@$ckan_sha#egg=ckan"
+
 $pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/$ckan_harvest_fork/ckanext-harvest/$ckan_harvest_sha/pip-requirements.txt)
 $pip install $pipopt -U "git+https://github.com/$ckan_harvest_fork/ckanext-harvest.git@$ckan_harvest_sha#egg=ckanext-harvest"
 
@@ -27,10 +31,6 @@ $pip install $pipopt -U "git+https://github.com/ckan/ckanext-dcat.git@$ckan_dcat
 
 $pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/$ckan_spatial_fork/ckanext-spatial/$ckan_spatial_sha/pip-requirements.txt)
 $pip install $pipopt -U "git+https://github.com/$ckan_spatial_fork/ckanext-spatial.git@$ckan_spatial_sha#egg=ckanext-spatial"
-
-$pip install $pipopt -U $(curl -s https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirement-setuptools.txt)
-$pip install $pipopt -r https://raw.githubusercontent.com/ckan/ckan/$ckan_sha/requirements.txt
-$pip install $pipopt -Ue "git+https://github.com/ckan/ckan.git@$ckan_sha#egg=ckan"
 
 $pip install $pipopt -r requirements.txt
 
