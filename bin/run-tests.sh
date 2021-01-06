8aaeb8865a187b8479294db6ebaf392b11ade67d
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-01-06 12:43:46 +0000

    Update run-tests for pytest
    Add code coverage report and ignore pytest warnings

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env sh
 
-if [ -z "$1" ]; then
-  test_name="ckanext.datagovuk"
-  coverage=" --with-coverage --cover-package=ckanext.datagovuk --cover-inclusive --cover-erase --cover-tests"
-else
-  test_name=$1
-  coverage=""
-fi
-
-# nosetests -v --nologcapture --with-pylons=${TEST_CKAN_INI:-test.ini} --with-randomly --ckan $coverage $test_name
-
-pytest --ckan-ini=test.ini $coverage $test_name
+pytest --ckan-ini=test.ini tests --cov=ckanext.datagovuk --cov-report=term-missing --disable-pytest-warnings -v
