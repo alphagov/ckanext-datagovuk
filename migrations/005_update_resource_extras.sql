f8af3bbebd39186d89ba0e0f3d7e521e3fad27ee
kentsang.gds <ken.tsang@digital.cabinet-office.gov.uk>
2021-09-08 19:52:41 +0100

    Update solr_reindex_target_date for resource

--- migrations/005_update_resource_extras.sql
+++ migrations/005_update_resource_extras.sql
@@ -1,4 +1,4 @@
 UPDATE resource 
 SET extras = '{"datafile-date": "", "resource-type": "data-link"}',
 last_modified = '2021-09-08' where id IN
-(SELECT id FROM resource WERE state = 'active' AND extras LIKE '%filename%');
+(SELECT id FROM resource WHERE state = 'active' AND extras LIKE '%filename%');
