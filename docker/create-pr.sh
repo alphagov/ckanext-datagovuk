4c9b30db117b0104ca36b9d5c36f34852b3a5b6e
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-06-22 11:38:04 +0100

    Update branch field in app yaml

--- docker/create-pr.sh
+++ docker/create-pr.sh
@@ -4,6 +4,7 @@ set -eux
 
 export IMAGE_TAG=$(gh api repos/alphagov/ckanext-datagovuk/branches/${GH_REF} | jq .commit.sha -r)
 BRANCH="ci/${IMAGE_TAG}"
+export GH_REF=${GH_REF}
 
 git config --global user.email "govuk-ci@users.noreply.github.com"
 git config --global user.name "govuk-ci"
@@ -18,6 +19,7 @@ for ENV in integration; do
     cd "${ENV}"
     for APP in ckan pycsw solr; do
       yq -i '.tag = env(IMAGE_TAG)' "${APP}.yaml"
+      yq -i '.branch = env(GH_REF)' "${APP}.yaml"
       git add "${APP}.yaml"
     done
     git commit -m "Update image tags for ${ENV} to ${IMAGE_TAG}"
