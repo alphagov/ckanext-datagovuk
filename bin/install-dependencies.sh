ba2f996e897db7748f7b9962b039d67397a8baff
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-07-07 15:17:45 +0100

    Update install dependencies to use 2.9.3 email fix

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -11,8 +11,8 @@ ckan_dcat_sha='be3b809fa7431c8d81508c01853e81ce6a5dfd84'
 ckan_spatial_fork='alphagov'
 ckan_spatial_sha='bac67ccb236b718d226ef5ac7844723222d3b5c5'
 
-# ckan 2.9.3 - alphagov/ckan/commits/fix-user-security
-ckan_sha='885f9e0b668e3496a8f2c0c0a9f1cb59bf810e16'
+# ckan 2.9.3 - alphagov/ckan/commits/fix-existing-email-validation
+ckan_sha='22613024e9a7303cb85438bd64ca67d168db5436'
 ckan_fork='alphagov'
 
 pycsw_tag='2.4.0'
