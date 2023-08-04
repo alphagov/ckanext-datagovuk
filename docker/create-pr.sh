2f2d346215c74cbd54bf75a284872d5d4922d1dc
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-08-04 18:55:06 +0100

    Fix the branch name to create and the directory

--- docker/create-pr.sh
+++ docker/create-pr.sh
@@ -9,7 +9,6 @@ else
   export IMAGE_TAG=$(gh api repos/alphagov/ckanext-datagovuk/branches/${GH_REF} | jq .commit.sha -r)
   export SOURCE_BRANCH=${GH_REF}
 fi
-BRANCH="ci/${IMAGE_TAG}"
 
 git config --global user.email "govuk-ci@users.noreply.github.com"
 git config --global user.name "govuk-ci"
@@ -17,11 +16,13 @@ git config --global user.name "govuk-ci"
 git clone https://${GH_TOKEN}@github.com/alphagov/govuk-ckan-charts.git charts
 
 cd charts/charts/ckan/images
-git checkout -b ${BRANCH}
 
 for ENV in $(echo $ENVS | tr "," " "); do
   (
-    cd "${ENV}"
+    BRANCH="ci/${IMAGE_TAG}-${ENV}"
+    git checkout -b ${BRANCH}
+
+    pushd "${ENV}"
     for APP in ckan pycsw solr; do
       yq -i '.tag = env(IMAGE_TAG)' "${APP}.yaml"
       yq -i '.branch = env(SOURCE_BRANCH)' "${APP}.yaml"
@@ -29,6 +30,7 @@ for ENV in $(echo $ENVS | tr "," " "); do
     done
     git commit -m "Update image tags for ${ENV} to ${IMAGE_TAG}"
     git push --set-upstream origin "${BRANCH}"
-    gh pr create --title "Update image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}-${ENV}" --fill
+    gh pr create --title "Update image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill
+    popd
   )
 done
