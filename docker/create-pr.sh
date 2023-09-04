e0ae7cb8d34b59fcdeb200d5ba629531cbb042b4
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2023-09-04 11:09:07 +0100

    Update create-pr.sh to handle duplicate branches

--- docker/create-pr.sh
+++ docker/create-pr.sh
@@ -20,16 +20,21 @@ cd charts/charts/ckan/images
 for ENV in $(echo $ENVS | tr "," " "); do
   (
     BRANCH="ci/${IMAGE_TAG}-${ENV}"
-    git checkout -b ${BRANCH}
 
-    cd "${ENV}"
-    for APP in ckan pycsw solr; do
-      yq -i '.tag = env(IMAGE_TAG)' "${APP}.yaml"
-      yq -i '.branch = env(SOURCE_BRANCH)' "${APP}.yaml"
-      git add "${APP}.yaml"
-    done
-    git commit -m "Update image tags for ${ENV} to ${IMAGE_TAG}"
-    git push --set-upstream origin "${BRANCH}"
-    gh pr create --title "Update image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill
+    if git show-ref --quiet refs/heads/${BRANCH}; then
+      echo "Branch ${BRANCH} already exists on govuk-ckan-charts"
+    else
+      git checkout -b ${BRANCH}
+
+      cd "${ENV}"
+      for APP in ckan pycsw solr; do
+        yq -i '.tag = env(IMAGE_TAG)' "${APP}.yaml"
+        yq -i '.branch = env(SOURCE_BRANCH)' "${APP}.yaml"
+        git add "${APP}.yaml"
+      done
+      git commit -m "Update image tags for ${ENV} to ${IMAGE_TAG}"
+      git push --set-upstream origin "${BRANCH}"
+      gh pr create --title "Update image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill
+    fi
   )
 done
