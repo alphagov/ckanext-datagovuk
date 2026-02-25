7f753dbf3d5fa54b63766a59820cbf617d908c9e
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2026-02-25 16:47:39 +0000

    Use alphagov fork of CKAN to fix smtp server connection

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Default git fork
-CKAN_FORK=ckan
-CKAN_SHA=9f2094b26721b28918d45d58fea16176df7a1ce1
+CKAN_FORK=alphagov
+CKAN_SHA=dc5a8a6e4ab290f619fc52c5a3880376d7b3883c
+CKAN_REPO=ckan-fix
 DCAT_FORK=ckan
 DCAT_SHA=618928be5a211babafc45103a72b6aab4642e964
 
@@ -11,13 +12,13 @@ SPATIAL_SHA=c4938431346b50209d7bcf89a1a0154698b9f9f2
 HARVEST_FORK=ckan
 HARVEST_SHA=9fb44f79809a1c04dfeb0e1ca2540c5ff3cacef4
 SRC_DIR=2.10
-FIND_SHA=v3.2.6
+FIND_SHA=v3.8.0
 
 echo -e "Please ensure that the ${SRC_DIR} docker/src directory is empty before running this command. This command will not populate the directories required for this project to run effectively unless said directories are already empty or don't exist.\n"
 
 mkdir -p docker/src/$SRC_DIR
 pushd docker/src/$SRC_DIR
-(git clone https://github.com/$CKAN_FORK/ckan && cd ckan && git checkout $CKAN_SHA)
+(git clone https://github.com/$CKAN_FORK/$CKAN_REPO && cd $CKAN_REPO && git checkout $CKAN_SHA)
 
 (git clone https://github.com/$HARVEST_FORK/ckanext-harvest && cd ckanext-harvest && git checkout $HARVEST_SHA)
 (git clone https://github.com/$SPATIAL_FORK/ckanext-spatial && cd ckanext-spatial && git checkout $SPATIAL_SHA)
